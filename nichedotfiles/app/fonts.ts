4b01850b7faf6f07dcea94528814e2434fe2d08c
Shom770 <shayaanwadkar@gmail.com>
2026-01-30 14:12:26 -0500

    rebasing

---
+++ nichedotfiles/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Rethink_Sans, Inter } from "next/font/google";
+
+export const rethinkSans = Rethink_Sans({
+  variable: "--font-rethink-sans",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
